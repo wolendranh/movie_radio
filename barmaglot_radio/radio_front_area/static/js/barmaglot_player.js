07f9401c00fcaccc6d087856f994f3a443562542
Yura <hulpa.yura@gmail.com>
2016-06-13 21:45:51 +0300

    added basiiiic player and jQuery 3.0 alpha :)

---
+++ barmaglot_radio/radio_front_area/static/js/barmaglot_player.js
@@ -0,0 +1,20 @@
+var Player = class Player {
+  constructor(playButton, pauseButton, playerContainer) {
+    let play = playButton;
+    let pause = pauseButton;
+    let player = playerContainer;
+
+    $(play).click(function () {
+      $(player)[0].play();
+      $(this).hide();
+      $(pause).show();
+
+    });
+
+    $(pause).click(function () {
+      $(player)[0].pause();
+      $(this).hide();
+      $(play).show();
+    });
+  }
+};
