1148361d28f9b45f7e5847d8e79cfa11788d09e2
yura.hulpa <hulpa.yura@gmail.com>
2016-07-22 18:32:20 +0300

    refactored Player to use React JS

--- static/js/barmaglot_player.js
+++ static/js/barmaglot_player.js
@@ -1,24 +1,115 @@
-"use strict";
+var Player = React.createClass({
 
-  var Player = class Player {
-    constructor(playButton, pauseButton, playerContainer) {
-      const play = playButton;
-      const pause = pauseButton;
-      const player = playerContainer;
+  setVolume: function(volume){
+    this.refs.audio.volume = volume;
+  },
 
-      $(pauseButton).hide();
+  getPlayer: function(){
+    return this.refs.audio;
+  },
 
-      $(play).click(function () {
-        $(player)[0].play();
-        $(this).hide();
-        $(pause).show();
+  render: function() {
+    return (
+      <div>
+          <Controls getPlayerRef={ this.getPlayer }/>
 
-      });
+          <div className="row player-container">
+              <div className="col-sm-4 col-sm-offset-4 player">
+                  <audio id="barmaglot-player" preload="none" ref="audio">
+                      <source src="http://195.248.234.62:8000/radioskovoroda" type="audio/mpeg"/>
+                  </audio>
+                  <div>
+                  </div>
+              </div>
 
-      $(pause).click(function () {
-        $(player)[0].pause();
-        $(this).hide();
-        $(play).show();
-      });
-    }
-  };
+              <Volume volumeHandle={ this.setVolume }/>
+          </div>
+      </div>
+    );}
+});
+
+
+var Controls = React.createClass({
+  getInitialState: function() {
+      return { showPause: false, showPlay: true };
+  },
+  displayPause: function(e){
+    this.setState({ showPause: true , showPlay: false });
+    var player = this.props.getPlayerRef();
+    player.play();
+  },
+  displayPlay: function(e){
+    this.setState({ showPause: false , showPlay: true });
+    var player = this.props.getPlayerRef();
+    player.pause();
+  },
+  render: function() {
+    return (
+      <div className="row player-controlls">
+              <div className="col-sm-4 col-sm-offset-4 shake">
+                  { this.state.showPause ? <div onClick={this.displayPlay } className="playback-pause center-block"></div> : null }
+
+                  { this.state.showPlay ? <div onClick={ this.displayPause } className="playback-play center-block"></div> : null }
+              </div>
+      </div>
+    )
+  }
+});
+
+var Volume = React.createClass({
+
+  propTypes: {
+      classes: React.PropTypes.array,
+      volumes: React.PropTypes.object
+  },
+
+  getDefaultProps: function() {
+    return {
+      classes: ['one', 'two', 'three', 'four', 'five'],
+      volumes: {'one': 0.2, 'two': 0.4, 'three': 0.6, 'four': 0.8, 'five': 1}
+    };
+  },
+
+  soundHandler: function(bar, event) {
+    var soundValue = this.props.volumes[bar];
+    console.log(soundValue);
+    this.props.volumeHandle(soundValue)
+  },
+
+  render: function () {
+    var getClass = function(i){
+      return 'volume-bar ' + i;
+    };
+
+    var _soundHandler = function(bar, event){
+        this.soundHandller(bar);
+    };
+
+    var _this = this, bars = this.props.classes.map(function(i) {
+      return <div onClick={ _this.soundHandler.bind(event, i) } className={getClass(i)}></div>;
+    });
+
+    return (
+    <div className="track-and-volume-wrapper">
+
+        <div className="trackInfoWrapper">
+            <div className="trackInfo">
+                   <p>pink floyd - wish you where here</p>
+            </div>
+        </div>
+
+        <div className="volume-bars-wrapper">
+            <div className="volume-bars">
+              {bars}
+            </div>
+        </div>
+
+    </div>
+    )
+  }
+});
+
+ReactDOM.render(
+  <Player />,
+  document.getElementById('barmaglot-player')
+);
