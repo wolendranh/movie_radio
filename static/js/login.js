0ab627288cfe70c9f226b92fc1c7950a91af1d03
yura.hulpa <hulpa.yura@gmail.com>
2016-07-25 18:04:41 +0300

    removed old login js

--- static/js/login.js
+++ static/js/login.js
@@ -1,33 +1,3 @@
-//$(document).ready(function(){
-//
-//    $('#signin').click(function(){
-//        window.location.href = "signin"
-//    });
-//
-//    function showError(error){
-//        $('#error').html(error)
-//    }
-//
-//    $('#submit').click(function(){
-//        var login = $('#login').val(),
-//            password = $('#password').val();
-//        console.log(login, 'if pass the same');
-//        if(login && password){
-//            $.post('login', {'login': login, 'password': password}, function(data){
-//                console.log(data);
-//                if (data.error){
-//                    showError(data.error)
-//                }else{
-//                    window.location.href = '/'
-//                }
-//            });
-//        }else{
-//            showError('Please fill all fields')
-//        }
-//    });
-//});
-
-
 var LoginForm = React.createClass({
     getInitialState: function() {
     return {password: 'Веедіть пароль',
