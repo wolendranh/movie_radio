a35bf1404551763b64a09c040b7183e2141cee51
Yura <hulpa.yura@gmail.com>
2016-08-02 09:37:14 +0300

    tokens in progresss

--- create_super_user.py
+++ create_super_user.py
@@ -5,7 +5,7 @@ from motor import motor_asyncio as ma
 from settings import MONGO_HOST, MONGO_DB_NAME
 
 
-async def create_super_user():
+async def create_super_user(*args, **kwargs):
     parser = argparse.ArgumentParser(description='Process new superuser data...')
     parser.add_argument('--email', type=str)
     parser.add_argument('--password', type=str)
@@ -13,8 +13,9 @@ async def create_super_user():
     params = parser.parse_args()
     mongo_client = ma.AsyncIOMotorClient(MONGO_HOST)
     mongo_db = mongo_client[MONGO_DB_NAME]
-    user = await User(db=mongo_db, data={'password': params['password'],
-                                         'email': params['email']}).create_user()
+    print('params', params)
+    user = await User(db=mongo_db, data={'password': params.password,
+                                         'email': params.email}).create_user()
     return user
 
 if __name__ == "__main__":
--- static/js/auth.jsx
+++ static/js/auth.jsx
@@ -1,12 +1,12 @@
 module.exports = {
     login: function(username, pass, cb) {
         if (localStorage.token) {
-            if (cb) cb(true)
-            return
+            if (cb) cb(true);
+                return
         }
         this.getToken(username, pass, (res) => {
             if (res.authenticated) {
-                localStorage.token = res.token
+                localStorage.token = res.token;
                 if (cb) cb(true)
             } else {
                 if (cb) cb(false)
