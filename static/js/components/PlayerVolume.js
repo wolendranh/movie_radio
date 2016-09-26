0f44bc0166b89586b7f8e20ddaae71446cdc7e8a
Yura <hulpa.yura@gmail.com>
2016-09-26 10:06:25 +0300

    local settings to ignore fixed

--- static/js/components/PlayerVolume.js
+++ static/js/components/PlayerVolume.js
@@ -1,11 +1,68 @@
-import {render} from 'react-dom';
-import React from 'react';
+import {render} from 'react-dom'
+import React from 'react'
+import $ from 'jquery';
+import 'jquery-ui/ui/widgets/slider';
+import 'jquery-ui/ui/widgets/draggable';
 
 var Volume = React.createClass({
 
   propTypes: {
       classes: React.PropTypes.array,
       volumes: React.PropTypes.object
+  },
+
+  componentDidMount: function () {
+    'use strict';
+        $('.slider').bind('mousewheel DOMMouseScroll', function (e) {
+            var delta = 0, element = $(this), value, result, oe;
+            oe = e.originalEvent; // for jQuery >=1.7
+            value = element.slider('value');
+
+            if (oe.wheelDelta) {
+                delta = -oe.wheelDelta;
+            }
+            if (oe.detail) {
+                delta = oe.detail * 40;
+            }
+
+            value -= delta / 8;
+            if (value > 100) {
+                value = 100;
+            }
+            if (value < 0) {
+                value = 0;
+            }
+
+            result = element.slider('option', 'slide').call(element, e, { value: value });
+            if (result !== false) {
+                element.slider('value', value);
+            }
+            return false;
+        });
+        var that = this;
+
+        var x = 10;
+        if(x => 0 && x <= 0.2){
+            console.log()
+        }else if (x > 0.2 && x <= 0.4){
+            console.log()
+        }
+
+        $(function () {
+            $('.slider').slider({
+                slide: function slide(event, ui) {
+                    var x = ui.value / 100;
+                    $('.value').val(x);
+                    var liquid= document.querySelector('.liquid');
+                    liquid.style.width = ui.value + '%';
+                    console.log(x);
+                    that.props.setVolumeHandle(x);
+                }
+
+            });
+            $('.slider').on('slide', (event, ui) => console.log(ui));
+            $('.slider').draggable();
+        });
   },
 
   getDefaultProps: function() {
@@ -15,6 +72,7 @@ var Volume = React.createClass({
       volumes: {'one': 0.2, 'two': 0.4, 'three': 0.6, 'four': 0.8, 'five': 1}
     };
   },
+
 
   soundHandler: function(bar, event) {
     var soundValue = this.props.volumes[bar];
@@ -34,9 +92,14 @@ var Volume = React.createClass({
   },
 
   render: function () {
-      var bars = this.props.classes.map((i, index) => {
-      return <div key={index} className={this.getClass(i)} onClick={(evt) => this.soundHandler(i, evt)}></div>;
-    });
+    //   var bars = this.props.classes.map((i, index) => {
+    //   return <div key={index} className={this.getClass(i)} onClick={(evt) => this.soundHandler(i, evt)}></div>;
+    // });
+      // svg styles
+      var style = {
+          fill: 'black',
+          fillOpacity: 0.1
+      };
 
     return (
     <div className="track-and-volume-wrapper">
@@ -46,12 +109,7 @@ var Volume = React.createClass({
                    <p className="current-song">{this.props.song }</p>
             </div>
         </div>
-
-        <div className="volume-bars-wrapper">
-            <div className="volume-bars">
-              {bars}
-            </div>
-        </div>
+        
 
     </div>
     )
