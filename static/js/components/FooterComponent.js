ab9beac1a8a712951427427d48b3831fc874329a
Yura <hulpa.yura@gmail.com>
2016-10-29 21:58:32 +0300

    added social

--- static/js/components/FooterComponent.js
+++ static/js/components/FooterComponent.js
@@ -7,15 +7,15 @@ var FooterComponent = React.createClass({
         return(<footer className="footer row-fluid">
                     <div className="col-sm-4 col-sm-offset-4">
                         <div className="row">
-                            <div className="social-button col-xs-4 col-sm-4 shake">
+                            <a className="social-button col-xs-4 col-sm-4 shake" href="https://www.facebook.com/barmaglot.radio" target="_blank">
                                 <img src="https://rawgit.com/wolendranh/movie_radio/master/static/img/social/fb.png" className="img-responsive"></img>
-                            </div>
-                            <div className="social-button col-xs-4 col-sm-4 shake">
+                            </a>
+                            <a className="social-button col-xs-4 col-sm-4 shake">
                                 <img src="https://rawgit.com/wolendranh/movie_radio/master/static/img/social/mail.png" className="img-responsive center-block"></img>
-                            </div>
-                            <div className="social-button col-xs-4 col-sm-4 shake">
+                            </a>
+                            <a className="social-button col-xs-4 col-sm-4 shake" href="https://vk.com/barmaglot.radio" target="_blank">
                                 <img src="https://rawgit.com/wolendranh/movie_radio/master/static/img/social/vk.png" className="img-responsive pull-right"></img>
-                            </div>
+                            </a>
                         </div>
                     </div>
                 </footer>)
