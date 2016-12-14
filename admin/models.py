c3f46eeab76443bc9c5d88cc6fd64d8be79c2593
ntodosiychuk <nata.todosiy@gmail.com>
2016-12-14 16:00:58 +0200

    Apply BaseModel for auth User model

--- admin/models.py
+++ admin/models.py
@@ -7,8 +7,7 @@ from radio_db.models import BaseModel
 class Stream(BaseModel):
     
     def __init__(self, db, data):
-        super().__init__(collection=db[STREAM_COLLECTION])
-        self.db = db
+        super().__init__(db, collection=db[STREAM_COLLECTION])
         self.stream_ip = data.get('stream_ip')
         self.name = data.get('name')
         self.id = data.get('id')
