73739a98a3f20c6bf9d4d35f9515a038f530117d
Yura <hulpa.yura@gmail.com>
2016-12-16 02:07:49 +0200

    added spinner for stream load

--- radio/services/date.py
+++ radio/services/date.py
@@ -4,10 +4,11 @@ from datetime import datetime
 MORNING = 'morning'
 DAY = 'day'
 EVENING = 'evening'
+# rely only on Local(ukrainian time). It need further investigation how to handle timezones
 KIEV_ZONE = 'Europe/Kiev'
 
 
-def get_day_time(day_time={}):
+def get_day_time():
     """
     Discover part of the day based on zone and hour
     Returns: `str`
@@ -16,32 +17,32 @@ def get_day_time(day_time={}):
 
     # small tests for local filter change
     # during test do not forged about day_time={}
-
-    if day_time.get('day_time'):
-        time = day_time.get('day_time')
-        if time == MORNING:
-            day_time['day_time'] = DAY
-        elif time == DAY:
-            day_time['day_time'] = EVENING
-        elif time == EVENING:
-            day_time['day_time'] = MORNING
-    else:
-        day_time['day_time'] = 'morning'
-
-
-    print('==' * 80)
-    print(day_time['day_time'])
-    print('==' * 80)
-    return day_time['day_time']
+    #
+    # if day_time.get('day_time'):
+    #     time = day_time.get('day_time')
+    #     if time == MORNING:
+    #         day_time['day_time'] = DAY
+    #     elif time == DAY:
+    #         day_time['day_time'] = EVENING
+    #     elif time == EVENING:
+    #         day_time['day_time'] = MORNING
+    # else:
+    #     day_time['day_time'] = 'morning'
+    #
+    #
+    # print('==' * 80)
+    # print(day_time['day_time'])
+    # print('==' * 80)
+    # return day_time['day_time']
 
     zone = pytz.timezone(KIEV_ZONE)
     hour = datetime.now(zone).hour
 
-    if 6 <= hour <= 12:
+    if 8 <= hour <= 12:
         time_of_day = MORNING
-    elif 12 <= hour <= 18:
+    elif 12 <= hour <= 17:
         time_of_day = DAY
-    elif 6 > hour or hour > 18:
+    elif 8 > hour or hour > 17:
         time_of_day = EVENING
 
     return time_of_day
--- static/js/components/PlayerComponent.js
+++ static/js/components/PlayerComponent.js
@@ -1,11 +1,13 @@
 import {render} from 'react-dom';
 import React from 'react';
 import $ from 'jquery';
+import Spinner from 'spin';
 
 import PlayerStore from "../stores/PlayerStore.js"
 import PlayerActions from "../actions/PlayerActions.js"
 import Controls from "./PlayerControls.js"
 import Volume from "./PlayerVolume.js"
+
 
 // TODO: fix filters
 import {triggerChange} from "../filters.js"
@@ -29,9 +31,27 @@ function getCurrentDatetime(){
     return PlayerStore.getDaytime()
 }
 
+function emitPlayerCanPlay(){
+    "use strict";
+    PlayerActions.canPlay();
+}
+
+function emitPlayerPlay(){
+    "use strict";
+    PlayerActions.play();
+}
+
 
 var Player = React.createClass({
-
+    getInitialState: function() {
+        return {
+            currentSong: 'Barmaglot ...',
+            stream: getActiveStream(),
+            spinnerActive: false
+        }
+    },
+    
+    
     componentDidMount: function() {
         PlayerStore.addChangeListener(this._onChange);
         PlayerStore.addTrackListener(this._onTrackUpdate);
@@ -46,20 +66,47 @@ var Player = React.createClass({
         FIRST_LOAD = false;
         PlayerActions.get();
 
-        setInterval(PlayerActions.getTrack, 300000);
+        setInterval(PlayerActions.getTrack, 3000);
+
+        // invoke spinner
+        this.controlSpinner();
+
+    },
+
+    handlePlayEvent: function(evt){
+        emitPlayerPlay();
+        this.spinner.spin(document.getElementById('spin-player'));
+
+    },
+    handleCanPlayEvent: function(evt){
+        emitPlayerCanPlay();
+        this.spinner.stop(document.getElementById('spin-player'));
+
+    },
+    controlSpinner: function(){
+        var player = this.getPlayer();
+        var opts = {
+            shadow: true,
+            radius: 25,
+            width: 11,
+            trail: 100,
+            length: 28,
+            lines: 13,
+            color: '#FFFFFF',
+            opacity: 0.5,
+            scale: 1
+        };
+        this.spinner = new Spinner(opts);
+
+        player.addEventListener('play', evt => this.handlePlayEvent(evt));
+        player.addEventListener('canplay', evt => this.handleCanPlayEvent(evt));
     },
 
     componentWillUnmount: function() {
         PlayerStore.removeChangeListener(this._onChange);
         PlayerStore.removeTrackListener(this._onTrackUpdate);
     },
-
-    getInitialState: function() {
-        return {
-          currentSong: 'Barmaglot ...',
-          stream: getActiveStream()
-        }
-    },
+    
 
     _onTrackUpdate: function (){
         "use strict";
@@ -82,7 +129,7 @@ var Player = React.createClass({
         this.setState({
             stream: getActiveStream()
         });
-        this.loadPlayer();
+        // this.loadPlayer();
     },
 
   getDefaultProps: function() {
@@ -106,10 +153,10 @@ var Player = React.createClass({
     }
   },
 
-  loadPlayer: function () {
-    var audio = this.getPlayer();
-    audio.load();
-  },
+  // loadPlayer: function () {
+  //   var audio = this.getPlayer();
+  //   audio.load();
+  // },
 
   getPlayer: function(){
     return this.refs.audio;
