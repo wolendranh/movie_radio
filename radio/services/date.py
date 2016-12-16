a2264c19b652ce5050a18e2b253885cccfdc0813
Yura <hulpa.yura@gmail.com>
2016-12-16 09:41:15 +0200

    demo

--- radio/services/date.py
+++ radio/services/date.py
@@ -8,7 +8,7 @@ EVENING = 'evening'
 KIEV_ZONE = 'Europe/Kiev'
 
 
-def get_day_time():
+def get_day_time(day_time={}):
     """
     Discover part of the day based on zone and hour
     Returns: `str`
@@ -18,22 +18,23 @@ def get_day_time():
     # small tests for local filter change
     # during test do not forged about day_time={}
     #
-    # if day_time.get('day_time'):
-    #     time = day_time.get('day_time')
-    #     if time == MORNING:
-    #         day_time['day_time'] = DAY
-    #     elif time == DAY:
-    #         day_time['day_time'] = EVENING
-    #     elif time == EVENING:
-    #         day_time['day_time'] = MORNING
-    # else:
-    #     day_time['day_time'] = 'morning'
-    #
-    #
-    # print('==' * 80)
-    # print(day_time['day_time'])
-    # print('==' * 80)
-    # return day_time['day_time']
+    if day_time.get('day_time'):
+        time = day_time.get('day_time')
+        if time == MORNING:
+            day_time['day_time'] = DAY
+        elif time == DAY:
+            day_time['day_time'] = EVENING
+        elif time == EVENING:
+            day_time['day_time'] = MORNING
+    else:
+        day_time['day_time'] = 'morning'
+
+
+    print('==' * 80)
+    print(day_time['day_time'])
+    print('==' * 80)
+    return day_time['day_time']
+    return day_time['day_time']
 
     zone = pytz.timezone(KIEV_ZONE)
     hour = datetime.now(zone).hour
