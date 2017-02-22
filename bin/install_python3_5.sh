7a3a15e6f6ecafbe5d1b2d0aa6843248fff2390c
Yura Hulpa <hulpa.yura@gmail.com>
2017-02-22 23:50:30 +0200

    Update install_python3_5.sh

--- bin/install_python3_5.sh
+++ bin/install_python3_5.sh
@@ -3,25 +3,30 @@
 ./
 
 PYTHON_ARCHIVE=Python-3.5.1.tar.xz
+PYTHON_DIR=Python-3.5.1
+FTP_SOURCE=https://www.python.org/ftp/python/3.5.1/Python-3.5.1.tar.xz
+
 echo "Starting install python 3.5..."
 
-echo $PYTHON_ARCHIVE
 
 if [ ! -f $PYTHON_ARCHIVE ]
+# skip downloading if archive already exists
 then
     echo "$PYTHON_ARCHIVE not found! Will try to download..."
     echo "Downloading..."
-    wget https://www.python.org/ftp/python/3.5.1/Python-3.5.1.tar.xz
+    wget $FTP_SOURCE
     [ $CODE -ne 0 ] && exit $CODE
 fi
 
 
 echo "Unpacking $PYTHON_ARCHIVE package..."
-tar xf Python-3.5.1.tar.xz
+tar xf $PYTHON_ARCHIVE
 [ $CODE -ne 0 ] && exit $CODE
 
 echo "Installion..."
-cd Python-3.5.1
+
+
+cd $PYTHON_DIR
 ./configure
 sudo make
 sudo make altinstall
