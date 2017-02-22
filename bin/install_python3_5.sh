df5ca6ec2aa90c3f2e3eb0d9aa2846de85a71db5
Yura Hulpa <hulpa.yura@gmail.com>
2017-02-22 23:22:16 +0200

    Update install_python3_5.sh

--- bin/install_python3_5.sh
+++ bin/install_python3_5.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+
 ./
+
+PYTHON_ARCHIVE=Python-3.5.1.tar.xz
 echo "Starting install python 3.5..."
 
-if [! -f Python-3.5.1.tar.xz ]; then
-    echo "Python archive not found! Will try to download..."
+echo $PYTHON_ARCHIVE
+
+if [ ! -f $PYTHON_ARCHIVE ]
+then
+    echo "$PYTHON_ARCHIVE not found! Will try to download..."
     echo "Downloading..."
     wget https://www.python.org/ftp/python/3.5.1/Python-3.5.1.tar.xz
     [ $CODE -ne 0 ] && exit $CODE
 fi
 
 
-echo "Unpacking package..."
+echo "Unpacking $PYTHON_ARCHIVE package..."
 tar xf Python-3.5.1.tar.xz
 [ $CODE -ne 0 ] && exit $CODE
 
