fcdb5e10a9a9aa5ad949595288e485b1f1573787
Yura <hulpa.yura@gmail.com>
2017-05-12 00:14:31 +0300

    test

--- radio_db/models.py
+++ radio_db/models.py
@@ -5,8 +5,7 @@ class BaseModel:
     """
     Base model to interact with Mongo DB
     """
-    # TODO: implement base functionality
-    # that should fit for all models
+    # TODO: implement base functionality that should fit for all models
 
     def __init__(self, db, collection):
         # Should use one db param and name of collection
